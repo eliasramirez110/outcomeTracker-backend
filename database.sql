01da3757bd57d5980d49d183961d2d5686be283d
Elias Ramirez <ramirezelias110@gmail.com>
2021-05-24 11:22:34 -0400

    backend redo

---
+++ database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE outcometrackerdb; 
+
+CREATE TABLE joblist(
+  joblist_id SERIAL PRIMARY KEY,
+  description VARCHAR(255)
+); 
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db")
 
 //middleware
 app.use(cors());
+app.use(express.json());
+
+//Routes
+
+//create a job
+app.post("/jobs",  async(req, res) => {
+  try {
+    
+    console.log(req.body)
+
+  } catch (error) {
+    console.error(err.messgae);
+  }
+})
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
